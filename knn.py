4a9dd7892248a46da7e54f9f352c6ec17320fc3d
tushar-c <37372851+tushar-c@users.noreply.github.com>
2018-03-14 20:14:45 +0530

    Update knn.py

--- knn.py
+++ knn.py
@@ -5,6 +5,7 @@ import random
 import process_data
 
 
+# get the k minimum entries
 def get_min(x, k=5):
     minimums = []
     data = [j[1] for j in x]
@@ -15,21 +16,26 @@ def get_min(x, k=5):
     return minimums
 
 
+# l2 norm
 def norm(x):
     return np.sqrt(np.sum(np.power(x, 2)))
 
 
 def knn(X, Y, test_features, test_labels, k=5):
     estimates = []
+    # go over the test set
     for t in range(len(test_features)):
         norms = []
+        # get distance between the vectors in terms of l2 norms ( over the whole train set)
         for x in X:
             norms.append((x, norm(test_features[t] - x)))
+        # get the k nearest neighbours
         k_neighbors = get_min(norms, k=k)
         total = 0
         for i in range(len(k_neighbors)):
             true_label = test_labels[t]
             estimate_label = Y[k_neighbors[i][2]]
+            # take the k nearest neighbours' majority vote and normalize
             total += estimate_label
             prediction = total / k
             if prediction >= 0.5:
@@ -37,11 +43,13 @@ def knn(X, Y, test_features, test_labels, k=5):
             elif prediction < 0.5:
                 estimates.append((0, true_label))
     sq_error = 0
+    # compute mse
     for e in estimates:
         sq_error += np.power(e[0] - e[1], 2)
     return sq_error / len(estimates)
 
 
+# fetch and clean the data
 pre_data = process_data.get_data()
 data = process_data.process_data(pre_data)
 features = data[0]
@@ -50,6 +58,7 @@ train_features = np.array(features[:80])
 train_labels = np.array(labels[:80]).reshape(80, 1)
 test_features = np.array(features[80:])
 test_labels = np.array(labels[80:]).reshape(20, 1)
+# manually set the number of nearest neighbours to consider
 K = 10
 knn_pred = knn(train_features, train_labels,
                 test_features, test_labels, k=K)
