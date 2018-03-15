corrected backprop

ffnn.py
@@ -1,5 +1,6 @@
 import process_data
 import numpy as np
+import matplotlib.pyplot as plt
 import time
 
 
@@ -23,6 +24,13 @@ def mse(y, y_):
     return np.sum(np.power(y - y_, 2))/len(y)
 
 
+def grad_mse(X, Y, Y_):
+    total = 0
+    for pt in range(len(Y)):
+        total += 2 * X[pt] * (Y_[pt] - Y[pt])
+    return total
+
+
 def gradient_descent_update_l1(x, grad, eta):
     return x - (eta * sigmoid(x)*(1 - sigmoid(x)))
 
@@ -31,44 +39,55 @@ def gradient_descent_update_l2(x, grad, eta):
     return x - (eta * sigmoid(x))
 
 
+def norm(x):
+    return np.power(np.sum(np.power(x, 2)), 1/2)
+
+
 def forward_pass(X, weights, biases):
+    activations = []
     f_1 = np.matmul(weights[0], X) + biases[0]
     af_1 = sigmoid(f_1)
+    activations.append(af_1)
     f_2 = np.matmul(weights[1], af_1) + biases[1]
     output = softmax(f_2)
-    return output
+    activations.append(output)
+    return [output, activations]
 
 
-def backward_pass(Y, weights, biases, eta):
-    Y = sigmoid(Y)
-    w_2 = np.matmul(weights[1].T, Y)
-    w_1 = np.matmul(weights[0].T, w_2)
-    weights[0] = gradient_descent_update_l1(weights[0], w_1, eta)
+def backward_pass(X, Y, Y_, activations, weights, biases, eta):
+    g = grad_mse(X, Y, Y_)
+    G = g * sigmoid(activations[1])
+    w_2 = np.matmul(G, activations[0].T)
     weights[1] = gradient_descent_update_l2(weights[1], w_2, eta)
-    biases[0] = gradient_descent_update_l1(biases[0], Y, eta)
-    biases[1] = gradient_descent_update_l2(biases[1], Y, eta)
+    g = np.matmul(weights[1].T, G)
+    w_1 = np.matmul(g, X.T)
+    weights[0] = gradient_descent_update_l1(weights[0], w_1, eta)
+    biases[0] = gradient_descent_update_l1(biases[0], G, eta)
+    biases[1] = gradient_descent_update_l2(biases[1], G, eta)
     weights = [weights[0], weights[1]]
     biases = [biases[0], biases[1]]
     return [weights, biases]
 
 
 def train_net(features, labels, weights, biases, eta=0.02, epochs=500):
-    error = 0
+    track_error = []
     learned_weights, learned_biases = [], []
     for e in range(epochs):
         correct = 0
+        error = 0
         print('train epoch {}'.format(e))
         for f in range(len(features)):
             votes = []
             data_point = features[f].ravel().reshape(features[f].shape[0], 1)
             predict = forward_pass(data_point, weights, biases)
-            gradients = backward_pass(predict, weights, biases, eta)
+            gradients = backward_pass(data_point, labels[f], predict[0], predict[1], weights, biases, eta)
             weights = gradients[0]
             biases = gradients[1]
-            error += mse(predict, labels[f])
+            error += mse(predict[0], labels[f])
+        track_error.append(mse(predict[0], labels[f]))
     learned_weights = weights
     learned_biases = biases
-    return (learned_weights, learned_biases, error)
+    return (learned_weights, learned_biases, error, track_error)
 
 
 # process_data
@@ -83,6 +102,7 @@ test_labels = np.array(labels[80:]).reshape(20, 1)
 train_features = normalize(train_features)
 test_features = normalize(test_features)
 
+
 # set up the architecture
 input_dim = 4
 hl_1_dim = 10
@@ -95,7 +115,7 @@ b2 = np.array(np.random.normal(size=output_dim), dtype=np.float64).reshape(outpu
 weights = [W1, W2]
 biases = [b1, b2]
 # train
-train = train_net(train_features, train_labels, weights, biases, eta= 0.001, epochs=100)
+train = train_net(train_features, train_labels, weights, biases, eta= 0.00001, epochs=100)
 # display results
 show_coeffs = False
 show_mse = False
@@ -113,10 +133,16 @@ n = len(test_labels)
 
 for d in range(len(test_labels)):
     predict = forward_pass(test_features[d], train[0], train[1])
-    votes = [p[0] for p in predict]
+    votes = [p[0] for p in predict[0]]
     predicted_class = votes.index(max(votes))
     if predicted_class == test_labels[d]:
         correct += 1
 
 accuracy = (correct / n) * 100
 print('test accuracy = {}%'.format(accuracy))
+plt_error = False
+if plt_error:
+    plt.plot(train[3])
+    plt.xlabel('training error over epochs')
+    plt.ylabel('mse over epochs')
+    plt.show()