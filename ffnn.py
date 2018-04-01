660dc099bd9a7f28573a776859a6e19443cc6e26
Tushar Chaturvedi <37372851+tushar-c@users.noreply.github.com>
2018-04-01 10:58:04 -0700

    Update ffnn.py

--- ffnn.py
+++ ffnn.py
@@ -1,6 +1,10 @@
+""" A dynamic feed-forward network, used for binary classificiation on the iris dataset for this particular example """
+
+
 import process_data
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 
 def normalize(X):
@@ -9,6 +13,10 @@ def normalize(X):
 
 def sigmoid(x):
     return 1/(1 + np.power(np.e, -x))
+
+
+def sigmoid_derivative(x):
+    return (1 - sigmoid(x)) * sigmoid(x)
 
 
 def softmax(x):
@@ -30,7 +38,7 @@ def grad_mse(X, Y, Y_):
     return total
 
 
-def gradient_descent_update_l1(x, grad, eta):
+def gradient_descent_update(x, grad, eta):
     return x - (eta * sigmoid(x)*(1 - sigmoid(x)))
 
 
@@ -42,51 +50,39 @@ def norm(x):
     return np.power(np.sum(np.power(x, 2)), 1/2)
 
 
+def backprop(y, y_, weights, biases, activations, eta):
+    g = mse(y, y_) * sigmoid(y_)
+    for i in range(len(weights) - 1, -1, -1):
+        grad_bias = g
+        grad_weights = np.matmul(g, activations[i - 1].T)
+        g = np.matmul(weights[i].T, g)
+    return [grad_weights, grad_bias]
+
+
 def forward_prop(X, weights, biases):
     activations = []
-    f_1 = np.matmul(weights[0], X) + biases[0]
-    af_1 = sigmoid(f_1)
-    activations.append(af_1)
-    f_2 = np.matmul(weights[1], af_1) + biases[1]
-    output = softmax(f_2)
-    activations.append(output)
-    return [output, activations]
+    inp = X.reshape(X.shape[0], 1)
+    for w in range(len(weights) - 1):
+        output = np.matmul(weights[w], inp) + biases[w]
+        activation = sigmoid(output)
+        activations.append(activation)
+        inp = activation
+    final_output = softmax(np.matmul(weights[-1], inp) + biases[-1])
+    activations.append(final_output)
+    return [final_output, activations]
 
 
-def backward_prop(X, Y, Y_, activations, weights, biases, eta):
-    g = grad_mse(X, Y, Y_)
-    G = g * sigmoid(activations[1])
-    w_2 = np.matmul(G, activations[0].T)
-    weights[1] = gradient_descent_update_l2(weights[1], w_2, eta)
-    g = np.matmul(weights[1].T, G)
-    w_1 = np.matmul(g, X.T)
-    weights[0] = gradient_descent_update_l1(weights[0], w_1, eta)
-    biases[0] = gradient_descent_update_l1(biases[0], G, eta)
-    biases[1] = gradient_descent_update_l2(biases[1], G, eta)
-    weights = [weights[0], weights[1]]
-    biases = [biases[0], biases[1]]
+def train_net(X, y, weights, biases, epochs=100, eta=0.002):
+    for e in range(1, epochs + 1):
+        print('train epoch = {}'.format(e))
+        for t in range(len(X)):
+            prediction = forward_prop(X[t], weights, biases)
+            activations = prediction[1]
+            gradients = backprop(y[t], prediction[0], weights, biases, activations, eta)
+            for i in range(len(weights)):
+                weights[i] = gradient_descent_update(weights[i], gradients[0], eta)
+                biases[i] = gradient_descent_update(biases[i], gradients[1], eta)
     return [weights, biases]
-
-
-def train_net(features, labels, weights, biases, eta=0.02, epochs=500):
-    track_error = []
-    learned_weights, learned_biases = [], []
-    for e in range(epochs):
-        correct = 0
-        error = 0
-        print('train epoch {}'.format(e))
-        for f in range(len(features)):
-            votes = []
-            data_point = features[f].ravel().reshape(features[f].shape[0], 1)
-            predict = forward_prop(data_point, weights, biases)
-            gradients = backward_prop(data_point, labels[f], predict[0], predict[1], weights, biases, eta)
-            weights = gradients[0]
-            biases = gradients[1]
-            error += mse(predict[0], labels[f])
-        track_error.append(mse(predict[0], labels[f]))
-    learned_weights = weights
-    learned_biases = biases
-    return (learned_weights, learned_biases, error, track_error)
 
 
 # process_data
@@ -94,9 +90,9 @@ pre_data = process_data.get_data()
 data = process_data.process_data(pre_data)
 features = data[0]
 labels = data[1]
-train_features = np.array(features[:80])
+train_features = np.array(features[:80]).reshape(80, 4)
 train_labels = np.array(labels[:80]).reshape(80, 1)
-test_features = np.array(features[80:])
+test_features = np.array(features[80:]).reshape(20, 4)
 test_labels = np.array(labels[80:]).reshape(20, 1)
 train_features = normalize(train_features)
 test_features = normalize(test_features)
@@ -104,15 +100,26 @@ test_features = normalize(test_features)
 
 # set up the architecture
 input_dim = 4
-hl_1_dim = 10
 output_dim = 2
+hidden_layers = 5
+hl_dimensions = [i*10 for i in range(1, 11)]
 # init weights and biases
-W1 = np.array(np.random.normal(size=hl_1_dim * input_dim), dtype=np.float64).reshape(hl_1_dim, input_dim)
-W2 = np.array(np.random.normal(size=output_dim * hl_1_dim), dtype=np.float64).reshape(output_dim, hl_1_dim)
-b1 = np.array(np.random.normal(size=hl_1_dim), dtype=np.float64).reshape(hl_1_dim, 1)
-b2 = np.array(np.random.normal(size=output_dim), dtype=np.float64).reshape(output_dim, 1)
-weights = [W1, W2]
-biases = [b1, b2]
+W1 = np.array(np.random.normal(size=hl_dimensions[0] * input_dim), dtype=np.float64).reshape(hl_dimensions[0], input_dim)
+b1 = np.array(np.random.normal(size=hl_dimensions[0])).reshape(hl_dimensions[0], 1)
+weights = [W1]
+biases = [b1]
+for i in range(1, hidden_layers):
+    curr_dim = hl_dimensions[i]
+    prev_dim = hl_dimensions[i - 1]
+    w_matrix = np.array(np.random.normal(size=curr_dim * prev_dim), dtype=np.float64).reshape(curr_dim, prev_dim)
+    b_matrix = np.array(np.random.normal(size=curr_dim), dtype=np.float64).reshape(curr_dim, 1)
+    biases.append(b_matrix)
+    weights.append(w_matrix)
+
+output_weights = np.array(np.random.normal(size=hl_dimensions[-1] * output_dim), dtype=np.float64).reshape(output_dim, hl_dimensions[-1])
+output_biases = np.array(np.random.normal(size=output_dim), dtype=np.float64).reshape(output_dim, 1)
+weights.append(output_weights)
+biases.append(output_biases)
 # train
 train = train_net(train_features, train_labels, weights, biases, eta= 0.00001, epochs=100)
 # display results
