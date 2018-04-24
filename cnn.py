2180d2efa20926be40d5017e248e0791199a6cac
Tushar Chaturvedi <37372851+tushar-c@users.noreply.github.com>
2018-04-24 03:27:57 -0700

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -1,5 +1,6 @@
 import numpy as np
 import ffnn
+import random
 
 
 def conv2d(img, kernel, bias, s, activation=False):
@@ -18,8 +19,8 @@ def conv2d(img, kernel, bias, s, activation=False):
     for x in range(spat_dim):
         for y in range(spat_dim):
             img_slice = img[x_spatial:k_x, y_spatial:k_y]
-            conv_out = np.sum(img_slice * kernel)
-            V[x, y] = conv_out + bias
+            conv_out = img_slice * kernel
+            V[x, y] = np.sum(conv_out) + bias
             y_spatial += s
             k_y += s
         x_spatial = x_spatial + s
@@ -28,6 +29,38 @@ def conv2d(img, kernel, bias, s, activation=False):
     if activation:
         V = ffnn.sigmoid(V)
     return V
+
+
+def max_pooling(img, pool_window, s):
+    x, y = img.shape[0], img.shape[1]
+    p_x, p_y = pool_window[0], pool_window[1]
+    if p_x > x or p_y > y or s > x or s > y:
+        print('Warning! Pool window size or stride is greater than input size')
+        print('pool window: {}, {}'.format(p_x, p_y))
+        print('stride = {}'.format(s))
+        print('input = {}, {}'.format(x, y))
+        print('returning None')
+        return None
+    spat_dim = int(np.floor((x - p_x) / s) + 1)
+    if spat_dim == 1:
+        pooled = 0
+    else:
+        pooled = np.full((spat_dim, spat_dim), 0)
+    x_spatial, y_spatial = 0, 0
+    for x in range(spat_dim):
+        for y in range(spat_dim):
+            img_slice = img[x_spatial:p_x, y_spatial:p_y]
+            pool_out = np.max(img_slice)
+            if spat_dim == 1:
+                pooled = pool_out
+            else:
+                pooled[x, y] = pool_out
+            y_spatial += s
+            p_y += s
+        x_spatial = x_spatial + s
+        p_x = p_x + s
+        y_spatial, p_y = 0, pool_window[1]
+    return pooled
 
 
 def avg_pooling(img, pool_window, s):
@@ -55,7 +88,10 @@ def avg_pooling(img, pool_window, s):
             if spat_dim == 1:
                 pooled = pool_out
             else:
-                pooled[x, y] = pool_out
+                try:
+                    pooled[x, y] = pool_out
+                except ValueError:
+                    print(img_slice)
             y_spatial += s
             p_y += s
         x_spatial = x_spatial + s
@@ -66,20 +102,6 @@ def avg_pooling(img, pool_window, s):
 
 def relu(x):
     return max(0, x)
-
-
-def conv_pass(inp, kernel, bias, strides, passes=1, show_flag=False):
-    outputs = []
-    for p in range(passes):
-        o = int(np.floor((inp.shape[0] - kernel[p].shape[0]) / strides) + 1)
-        conv_pass = conv2d(inp, kernel[p], bias[p], strides)
-        o_pool = int(np.floor((o - kernel[p].shape[0]) / strides) + 1)
-        if show_flag:
-            print('after pooling pass {}, input shape : {}, output shape : {}'.format(p+1, inp.shape, (o_pool, o_pool)))
-        pooled_conv = avg_pooling(conv_pass, (2, 2), strides)
-        outputs.append(avg_pooling(conv_pass, (2, 2), strides))
-        inp = pooled_conv
-    return inp
 
 
 def activation(x):
@@ -142,17 +164,45 @@ def upscale(curr_array, desired_array):
     return dupl_desired_array / 4
 
 
+def infer_output_layer_shape(input_shape, conv_layers, kernels, poolings, kernel_strides=1, pool_stride=2):
+    curr_input_shape = input_shape
+    for c in range(conv_layers):
+        conv_shape = int(np.floor((curr_input_shape - kernels[c].shape[0])/kernel_strides) + 1)
+        pool_shape = int(np.floor((conv_shape - poolings[c][0])/pool_stride) + 1)
+        curr_input_shape = pool_shape
+    return curr_input_shape
+
+
+def forward_conv_pass(input_, conv_layers, kernels, biases, poolings, kernel_strides=1, pool_stride=2):
+    curr_input = input_
+    convolution_cache = []
+    activation_cache = []
+    pooling_cache = []
+    for c in range(conv_layers):
+        convolution = conv2d(curr_input, kernels[c], biases[c], kernel_strides)
+        activated_convolution = sigmoid(convolution)
+        pooling = avg_pooling(activated_convolution, poolings[c], pool_stride)
+        convolution_cache.append(convolution)
+        activation_cache.append(activated_convolution)
+        pooling_cache.append(pooling)
+        curr_input = pooling
+    return [convolution_cache, activation_cache, pooling_cache]
+
+
 if __name__ == "__main__":
-    # init stuff
+    # init hyperparams
+    conv_layers = 2
     output_classes = 2
-    eta = 0.1
-    passes = 100
+    eta = 0.5
+    passes = 5
     x_input, y_input = 28, 28
     x_kernel, y_kernel = 5, 5
-    pool_stride = 2
+    pool_stride = 1
     pool_window = [2, 2]
     conv_stride = 1
-    dataset_size = 200
+    kernel_stride = 1
+    dataset_size = 10
+    print('init data and labels')
     dummy_data = [np.array([np.random.normal() for i in range(x_input * y_input)]).reshape(x_input, y_input) for j in range(dataset_size)]
     dummy_answers = []
     for k in range(len(dummy_data)):
@@ -162,53 +212,63 @@ if __name__ == "__main__":
         else:
             d_lbl = np.array([1, 0]).reshape(2, 1)
         dummy_answers.append(d_lbl)
-    y = np.array([1, 0]).reshape(2, 1)
-    k1 = np.array([np.random.normal() for i in range(x_kernel * y_kernel)]).reshape(x_kernel, y_kernel)
-    k2 = np.array([np.random.normal() for i in range(x_kernel * y_kernel)]).reshape(x_kernel, y_kernel)
-    bias_1, bias_2 = 0.5, 0.3
-    output_layer_weights = np.array([np.random.normal() for i in range(output_classes * 16)]).reshape(output_classes, 16)
+
+    print('data init complete, init kernels and biases')
+    kernels = [np.array([np.random.normal() for i in range(x_kernel * y_kernel)], dtype=np.float64).reshape(x_kernel, y_kernel)\
+               for k in range(conv_layers)]
+    poolings = [pool_window for i in range(conv_layers)]
+    biases = [np.random.normal() for i in range(conv_layers)]
+    print('init kernels and biases complete')
+    final_output_shape = infer_output_layer_shape(x_input, conv_layers, kernels, poolings)
+    output_layer_weights = np.array([np.random.normal() for i in range(output_classes * (final_output_shape**2))]).reshape(output_classes, final_output_shape**2)
     output_layer_biases = np.array([0 for i in range(output_classes)]).reshape(output_classes, 1)
+    print('init learning....')
     for p in range(passes):
         pass_mse = 0
         pass_cross_entropy = 0
         pass_accuracy = 0
         for d in range(len(dummy_data)):
             # forward pass
-            c1 = conv2d(dummy_data[d], k1, bias_1, 1)
-            ac1 = sigmoid(c1)
-            pool1 = avg_pooling(ac1, pool_window, pool_stride)
-            c2 = conv2d(pool1, k2, bias_2, conv_stride)
-            ac2 = sigmoid(c2)
-            pool2 = avg_pooling(ac2, pool_window, pool_stride)
-            # the 'layers' are actually weight matrices
-            fc_layer = np.ravel(pool2).reshape(pool2.shape[0] * pool2.shape[1], 1)
+            forward_pass = forward_conv_pass(dummy_data[d], conv_layers, kernels, biases, poolings)
+            convolution_cache, activation_cache, pooling_cache = forward_pass[0], forward_pass[1], forward_pass[2]
+            fc_result = pooling_cache[-1]
+            fc_layer = np.ravel(fc_result).reshape(fc_result.shape[0] * fc_result.shape[1], 1)
             final_output = softmax(np.matmul(output_layer_weights, fc_layer) + output_layer_biases)
-            error = (final_output - dummy_answers[d]) * sigmoid(final_output)
             prediction = list(final_output).index(max(final_output))
             if prediction == list(dummy_answers[d]).index(max(dummy_answers[d])):
                 pass_accuracy += 1
             pass_mse += mse(prediction, dummy_answers[d])
             # backward pass
+            # here, the last conv backprop is manual
+            error = (final_output - dummy_answers[d]) * final_output * (1 - final_output)
             delta_output_layer_weights = np.matmul(error, fc_layer.T)
+            delta_fully_connected = np.matmul(output_layer_weights.T, error)
+            delta_final_pool = fc_layer.reshape(final_output_shape, final_output_shape)
+            delta_final_conv = upscale(delta_final_pool, convolution_cache[-1])
+            delta_final_conv_sigma = delta_final_conv * convolution_cache[-1] * (1 - convolution_cache[-1])
+            delta_final_kernel = conv2d(np.rot90(pooling_cache[-2], 2), delta_final_conv_sigma, 0.3, kernel_stride)
+            #print(pooling_cache[-2], delta_final_conv_sigma, delta_final_kernel)
+            delta_final_bias = np.sum(delta_final_conv_sigma)
+            # automatic now
+            backward_kernel_cache = []
+            backward_bias_cache = []
+            delta_conv_sigma_cache = []
+            curr_delta_conv_sigma = delta_final_conv_sigma
+            curr_delta_pool = delta_final_pool
+            # we add the original input to the pooling to automate the last backward pass
+            pooling_cache.append(dummy_data[d])
+            for j in range(conv_layers - 2, -1, -1):
+                delta_conv_sigma = curr_delta_conv_sigma
+                delta_curr_pool = conv2d(delta_conv_sigma, np.rot90(kernels[j + 1], 2), biases[j + 1], kernel_stride)
+                delta_curr_conv = upscale(delta_curr_pool, convolution_cache[j])
+                curr_delta_conv_sigma = delta_curr_conv * convolution_cache[j] * (1 - convolution_cache[j])
+                delta_curr_kernel = conv2d(np.rot90(pooling_cache[j - 1], 2), curr_delta_conv_sigma, 0, kernel_stride)
+                delta_curr_bias = np.sum(curr_delta_conv_sigma)
+                kernels[j] = gradient_descent_update(kernels[j], delta_curr_kernel, eta)
+                biases[j] = gradient_descent_update(biases[j], delta_curr_bias, eta)
+                backward_kernel_cache.append(delta_curr_kernel)
+                backward_bias_cache.append(delta_curr_bias)
+                delta_conv_sigma_cache.append(delta_conv_sigma)
             output_layer_weights = gradient_descent_update(output_layer_weights, delta_output_layer_weights, eta)
-            output_layer_biases = gradient_descent_update(output_layer_biases, error, eta)
-            pool2_error = np.matmul(output_layer_weights.T, error)
-            matrix_pool2_error = pool2_error.reshape(pool2.shape[0], pool2.shape[1])
-            # upscale
-            delta_c2 = upscale(matrix_pool2_error, c2)
-            activated_delta_c2 = sigmoid(delta_c2)
-            delta_k2 = conv2d(np.rot90(pool1, 2), activated_delta_c2, bias_2, 1)
-            # kernel2 update
-            k2 = gradient_descent_update(k2, delta_k2, eta)
-            # bias2 update
-            bias_2 = np.sum(activated_delta_c2)
-            pool1_error = conv2d(activated_delta_c2, np.rot90(k2, 2), bias_1, 1)
-            delta_c1 = upscale(pool1_error, c1)
-            activated_delta_c1 = sigmoid(delta_c1)
-            delta_k1 = conv2d(np.rot90(dummy_data[d], 2), activated_delta_c1, bias_1, 1)
-            # kernel1 update
-            k1 = gradient_descent_update(k1, delta_k1, eta)
-            bias_1 = np.sum(activated_delta_c1)
-        print('pass = {}, pass_mse = {}, correct predictions = {} ,total observations = {}, pass_accuracy = {}'\
-              .format(p + 1, pass_mse, pass_accuracy, len(dummy_answers), pass_accuracy / len(dummy_answers)))
-
+            output_layer_biases = error
+        print('pass = {}, pass_mse = {}, correct predictions = {}, pass_accuracy = {}'.format(p + 1, pass_mse, pass_accuracy, pass_accuracy / len(dummy_answers)))
