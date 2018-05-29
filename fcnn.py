7254d6b01b0cbb50d174ad80d02ec18b9781dd59
Tushar Chaturvedi <37372851+tushar-c@users.noreply.github.com>
2018-05-29 16:16:56 +0530

    Update fcnn.py

--- fcnn.py
+++ fcnn.py
@@ -1,22 +1,30 @@
 """ A dynamic feed-forward network, used for binary classificiation on the iris dataset for this particular example. """
 
-
-import process_data
+import conv_utils
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_boston
+import keras
+from keras.datasets import mnist
 
 
 def normalize(X):
-    return (X - np.mean(X))/np.std(X)
+    return (X - np.mean(X)) / np.std(X)
 
 
 def sigmoid(x):
-    return 1/(1 + np.power(np.e, -x))
+    return conv_utils.stable_sigmoid(x)
+
+
+def grad_sigmoid(x):
+    return sigmoid(x) * (1 - sigmoid(x))
 
 
 def softmax(x):
-    return np.power(np.e, x)/np.sum(np.power(np.e, x))
+    return np.power(np.e, x) / np.sum(np.power(np.e, x))
+
+
+def grad_softmax(x):
+    return softmax(x) * (1 - softmax(x))
 
 
 def predict(X, w):
@@ -24,19 +32,20 @@ def predict(X, w):
 
 
 def mse(y, y_):
-    return np.sum(np.power(y - y_, 2))/len(y)
+    return np.sum(np.power(y - y_, 2)) / 2
 
 
 def grad_mse(y, y_):
-    return -np.sum((y - y_))
+    return np.sum(y_ - y)
 
 
 def gradient_descent_update(x, grad, eta):
-    return x - (eta * sigmoid(x)*(1 - sigmoid(x)))
+    return x - (eta * grad)
 
 
 def norm(x):
-    return np.power(np.sum(np.power(x, 2)), 1/2)
+    return np.power(np.sum(np.power(x, 2)), 1 / 2)
+
 
 def relu(x):
     return max(0, x)
@@ -60,77 +69,98 @@ def accuracy(test_features, test_labels, weights, biases):
     return accuracy
 
 
-def backprop(y, y_, weights, biases, activations, eta):
-    g = grad_mse(y, y_) * sigmoid(y_)
-    for i in range(len(weights) - 1, -1, -1):
+def backprop(y, y_, preactivated_output, weights, biases, activations, eta):
+    y = y.reshape(y_.shape[0], 1)
+    g = (y_ - y) * conv_utils.sigmoid_gradient(preactivated_output)
+    grad_weight_cache, grad_bias_cache = [], []
+    for i in range(len(weights) - 1, 0, -1):
         grad_bias = g
-        grad_weights = np.matmul(g, activations[i - 1].T)
+        grad_weights = np.matmul(g, activations[i].T)
+        grad_weight_cache.append(grad_weights)
+        grad_bias_cache.append(grad_bias)
         g = np.matmul(weights[i].T, g)
-    return [grad_weights, grad_bias]
+    grad_bias_cache.append(g)
+    grad_weight_cache.append(np.matmul(g, activations[0].T))
+    final_grad_weight_cache = [i for i in reversed(grad_weight_cache)]
+    final_grad_bias_cache = [j for j in reversed(grad_bias_cache)]
+    return [final_grad_weight_cache, final_grad_bias_cache]
 
 
 def forward_prop(X, weights, biases):
-    activations = []
-    inp = X.reshape(X.shape[0], 1)
+    inp = X.reshape(X.shape[0] * X.shape[1], 1)
+    activations = [inp]
     for w in range(len(weights) - 1):
         output = np.matmul(weights[w], inp) + biases[w]
-        activation = sigmoid(output)
+        activation = conv_utils.stable_sigmoid(output)
         activations.append(activation)
         inp = activation
-    final_output = softmax(np.matmul(weights[-1], inp) + biases[-1])
+    preactivated_output = np.matmul(weights[-1], inp) + biases[-1]
+    final_output = conv_utils.stable_sigmoid(preactivated_output)
     activations.append(final_output)
-    return [final_output, activations]
+    return [final_output, activations, preactivated_output]
 
 
-def train_net(X, y, weights, biases, epochs=100, eta=0.002):
+def train_net(X, y, X_test, y_test, weights, biases, epochs=12, eta=0.002):
     accuracy_list = []
     hyperparam_tracker = []
+    mse_tracker = []
     for e in range(1, epochs + 1):
         epoch_mse = 0
-        print('train epoch = {}'.format(e), end = ", ")
         for t in range(len(X)):
+            print('epoch {} / {} ; sample {} / {}'.format(e, epochs, t + 1, len(X)))
             prediction = forward_prop(X[t], weights, biases)
             activations = prediction[1]
-            epoch_mse += mse(y[t], prediction[0])
-            gradients = backprop(y[t], prediction[0], weights, biases, activations, eta)
+            epoch_mse += mse(np.argmax(y[t]), np.argmax(prediction[0]))
+            gradients = backprop(y[t], prediction[0], prediction[2], weights, biases, activations, eta)
             for i in range(len(weights)):
-                weights[i] = gradient_descent_update(weights[i], gradients[0], eta)
-                biases[i] = gradient_descent_update(biases[i], gradients[1], eta)
+                weights[i] = gradient_descent_update(weights[i], gradients[0][i], eta)
+            for b in range(len(biases)):
+                biases[b] = gradient_descent_update(biases[b], gradients[1][b], eta)
+        print('epoch {} / {} ; evaluating accuracy'.format(e, epochs))
         acc = accuracy(X, y, weights, biases)
         accuracy_list.append(acc)
         hyperparam_tracker.append((acc, weights, biases))
         print('train accuracy = {}%'.format(acc), end=", ")
-        print('mse = {}'.format(epoch_mse))
-    print('max train accuracy was {}%'.format(max(accuracy_list)))
+        print('epoch mse = {}'.format(epoch_mse))
+        mse_tracker.append(epoch_mse)
+    print('max train accuracy was found to be {}%'.format(max(accuracy_list)))
     accs = accuracy_list.index(max(accuracy_list))
     highest_acc_params = hyperparam_tracker[accs]
     max_acc_w_b = [highest_acc_params[1], highest_acc_params[2]]
-    return [weights, biases, max_acc_w_b]
+    return [weights, biases, max_acc_w_b, mse_tracker, accuracy_list]
 
 
 if __name__ == '__main__':
     # process_data
-    pre_data = process_data.get_data()
-    data = process_data.process_data_2(pre_data)
-    features = data[0]
-    labels = data[1]
-    train_features = np.array(features[:80]).reshape(80, 4)
-    test_features = np.array(features[80:]).reshape(20, 4)
-    train_labels = labels[:80]
-    test_labels = labels[80:]
-    train_features = normalize(train_features)
-    test_features = normalize(test_features)
-
+    # input image dimensions
+    img_rows, img_cols = 28, 28
+    num_classes = 10
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    train_features = x_train
+    test_features = x_test
+    train_labels = y_train
+    test_labels = y_test
+    train_features /= 255
+    test_features /= 255
+    train_labels = keras.utils.to_categorical(train_labels, num_classes)
+    test_labels = keras.utils.to_categorical(test_labels, num_classes)
+    test_labels = test_labels.reshape(10000, 10)
+    train_labels = train_labels.reshape(60000, 10)
+    train_features = train_features[:10000]
+    train_labels = train_labels[:10000]
 
     # set up the architecture and hyperparameters
-    epochs = 1000
-    eta = 0.002
-    input_dim = train_features.shape[1]
-    output_dim = 2
-    hidden_layers = 5
-    hl_dimensions = [50, 40, 40, 30, 30]
+    epochs = 30
+    eta = 0.0275
+    input_dim = 784
+    output_dim = 10
+    hidden_layers = 1
+    hl_dimensions = [30]
     # init weights and biases
-    W1 = np.array(np.random.normal(size=hl_dimensions[0] * input_dim), dtype=np.float64).reshape(hl_dimensions[0], input_dim)
+    W1 = np.array(np.random.normal(size=hl_dimensions[0] * input_dim), dtype=np.float64).reshape(hl_dimensions[0],
+                                                                                                 input_dim)
     b1 = np.array(np.random.normal(size=hl_dimensions[0])).reshape(hl_dimensions[0], 1)
     weights = [W1]
     biases = [b1]
@@ -142,7 +172,9 @@ if __name__ == '__main__':
         biases.append(b_matrix)
         weights.append(w_matrix)
 
-    output_weights = np.array(np.random.normal(size=hl_dimensions[-1] * output_dim), dtype=np.float64).reshape(output_dim, hl_dimensions[-1])
+    output_weights = np.array(np.random.normal(size=hl_dimensions[-1] * output_dim), dtype=np.float64).reshape(output_dim,
+                                                                                                               hl_dimensions[
+                                                                                                                   -1])
     output_biases = np.array(np.random.normal(size=output_dim), dtype=np.float64).reshape(output_dim, 1)
     weights.append(output_weights)
     biases.append(output_biases)
@@ -157,10 +189,11 @@ if __name__ == '__main__':
         print('input layer : shape = ({}, 1)'.format(input_dim))
         print('hidden layer {}, weights: {} , biases : {}'.format(1, (hl_dimensions[0], input_dim), hl_dimensions[0]))
         for i in range(1, hidden_layers):
-            print('hidden layer {}, weights: {} , biases : {}'.format(i + 1, (hl_dimensions[i], hl_dimensions[i-1]), hl_dimensions[i]))
+            print('hidden layer {}, weights: {} , biases : {}'.format(i + 1, (hl_dimensions[i], hl_dimensions[i - 1]),
+                                                                      hl_dimensions[i]))
         print('output layer: weights: {} , biases : {}'.format((output_dim, hl_dimensions[-1]), hl_dimensions[-1]))
     print('-' * 50)
-    train = train_net(train_features, train_labels, weights, biases, eta=eta, epochs=epochs)
+    train = train_net(train_features, train_labels, test_features, test_labels, weights, biases, eta=eta, epochs=epochs)
     print()
 
     # display results
@@ -176,8 +209,11 @@ if __name__ == '__main__':
     show_test_mse = True
     test_mse = 0
     for d in range(len(test_labels)):
-        """ using max acc weights and biases. to use last found weights and biases,
-            predict = forward_prop(test_features[d], train[0], train[1])
+        """ 
+        using max accuracy weights and biases. to use last found weights and biases, do:
+        predict = forward_prop(test_features[d], train[0], train[1])
+        for max weights and biases, do:
+        predict = forward_prop(test_features[d], train[2][0], train[2][1])
         """
         predict = forward_prop(test_features[d], train[2][0], train[2][1])
         test_mse += mse(test_labels[d], predict[0])
@@ -191,9 +227,15 @@ if __name__ == '__main__':
     if show_test_mse:
         print('test mse = {}'.format(test_mse))
     print('test accuracy = {}%'.format(accuracy))
-    plt_error = False
+    plt_error = True
+    plt_accuracy = True
     if plt_error:
         plt.plot(train[3])
-        plt.xlabel('training error over epochs')
+        plt.xlabel('epochs')
         plt.ylabel('mse over epochs')
+        plt.show()
+    if plt_accuracy:
+        plt.plot(train[4])
+        plt.xlabel('epochs')
+        plt.ylabel('train accuracy over epochs')
         plt.show()
